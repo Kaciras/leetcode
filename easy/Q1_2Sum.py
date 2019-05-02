735732ca39a8a64148a9b26626151bd46df3fe8d
Kaciras <Kaciras@outlook.com>
2019-05-02 21:41:13 +0800

    optimize 2Sum

--- easy/Q1_2Sum.py
+++ easy/Q1_2Sum.py
@@ -1,18 +1,25 @@
+from typing import List
+
+
+# 虽然本地快了点，但在leetcode上还是56ms
 class Solution:
 
-	def twoSum(self, nums, target):
+	def twoSum(self, nums: List[int], target: int) -> List[int]:
 		"""
-		:type nums: List[int]
-		:type target: int
-		:rtype: List[int]
+		直接 key in dict 而不是 key in dict.keys()，耗时降低了 0.1 秒/百万次
+		使用 enumerate(nums) 代替 range(len(nums)))，耗时降低了 0.07 秒/百万次
+		visited = dict() 比 visited = {} 写法竟然慢了 0.1 秒/百万次
 		"""
-		visited = dict()
-		for i in range(len(nums)):
-			n = nums[i]
+		visited = {}
+		for i, n in enumerate(nums):
 			another = target - n
-			if another in visited.keys():
+			if another in visited:
 				return [visited[another], i]
 			visited[n] = i
 
+
 if __name__ == '__main__':
-	print(Solution().twoSum([2, 7, 11, 15], 9))
+	print(Solution().twoSum([2, 1, -8, 7, 11, 15], 9))
+	import timeit
+
+	print(timeit.repeat("Solution.twoSum(None, [2, 7, 11, 15], 9)", globals=globals()))
