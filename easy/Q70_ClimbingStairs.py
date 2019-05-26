7f66d2c7c5cdf3d48da100a0e292e00ca0dfc7f1
Kaciras <Kaciras@outlook.com>
2019-05-26 10:54:56 +0800

    Q70: fix index error

--- easy/Q70_ClimbingStairs.py
+++ easy/Q70_ClimbingStairs.py
@@ -1,11 +1,8 @@
 class Solution:
 
-	def climbStairs(self, n):
-		"""
-		:type n: int
-		:rtype: int
-		"""
-		if n < 2: return n
+	def climbStairs(self, n: int) -> int:
+		if n < 2:
+			return n
 
 		a, b, = 1, 2
 		while n > 2:
