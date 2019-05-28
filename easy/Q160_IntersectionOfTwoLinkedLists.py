18e9aee34636253355360e4e1fd18a027524eee9
Kaciras <Kaciras@outlook.com>
2019-05-28 22:26:01 +0800

    optimize 160

--- easy/Q160_IntersectionOfTwoLinkedLists.py
+++ easy/Q160_IntersectionOfTwoLinkedLists.py
@@ -1,36 +1,38 @@
-from utils import linked_list, ListNode, print_linked_list
+from utils import linked_list, benckmark
 
 
 class Solution:
-	def getIntersectionNode(self, headA, headB):
-		"""
-		:type headA: ListNode
-		:type headB: ListNode
-		:rtype: ListNode
-		"""
-		def count(head):
-			r = 0
-			while head:
-				r += 1
-				head = head.next
-			return r
+	"""这题没有Py3，不能在参数上定义类型"""
 
-		def next_of(node, n):
-			for i in range(n):
-				node = node.next
-			return node
+	def getIntersectionNode(self, ha, hb):
+		"""
+		先求出两个链表的长度，以及它们的差，然后让长的先跑长度差个，
+		再两个一起跑，此时它们一定会在交点处相遇，或是同时到达尾部。
+		"""
 
-		countA, countB = count(headA), count(headB)
-		if countA > countB:
-			headA = next_of(headA, countA - countB)
+		ca, cb = self.length(ha), self.length(hb)
+		if ca > cb:
+			ha = self.next_of(ha, ca - cb)
 		else:
-			headB = next_of(headB, countB - countA)
+			hb = self.next_of(hb, cb - ca)
 
-		while headA:
-			if headA == headB:
-				return headA
-			headA, headB = headA.next, headB.next
+		while ha:
+			if ha == hb: return ha
+			ha, hb = ha.next, hb.next
+
 		return None
+
+	def length(self, head):
+		r = 0
+		while head:
+			r += 1
+			head = head.next
+		return r
+
+	def next_of(self, node, n):
+		for i in range(n):
+			node = node.next
+		return node
 
 
 if __name__ == '__main__':
@@ -39,3 +41,5 @@ if __name__ == '__main__':
 			  linked_list(["c1", "c2", "c3"])
 	a.next.next = b.next.next.next = c
 	print(Solution().getIntersectionNode(a, b))
+
+	benckmark(Solution().getIntersectionNode, a, b, ratio=1)
