86469e8787ae1ce7f202f1d24828b29b81bc7c52
Kaciras <Kaciras@protonmail.com>
2024-09-02 13:29:18 +0800

    add question 27

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 mod q6_zigzag_conversion;
 mod q9_palindrome_number;
+mod q27_remove_elements;
 
 fn main() {
 
---
+++ src/q27_remove_elements.rs
@@ -0,0 +1,40 @@
+pub struct Solution {}
+
+// 操蛋，看别人的写法整二维数组的竟然比我这一维的还快，看来是我给搞复杂了。
+impl Solution {
+	pub fn remove_element(nums: &mut Vec<i32>, val: i32) -> i32 {
+		let mut k = nums.len();
+		let mut i = 0;
+		while i < k {
+			if nums[i] == val {
+				k -= 1;
+				nums[i] = nums[k];
+			} else {
+				i += 1;
+			}
+		}
+		return k as i32;
+	}
+}
+
+#[cfg(test)]
+mod tests {
+	use super::*;
+
+	#[test]
+	fn example1() {
+		let mut array = vec![3, 2, 2, 3];
+		let size = Solution::remove_element(&mut array, 3);
+		assert_eq!(size, 2);
+		assert_eq!(array[0..size as usize], vec![2, 2]);
+	}
+
+	#[test]
+	fn example2() {
+		let mut array = vec![0,1,2,2,3,0,4,2];
+		let size = Solution::remove_element(&mut array, 2);
+		assert_eq!(size, 5);
+		assert_eq!(array[0..size as usize], vec![0,1,4,0,3]);
+	}
+
+}
