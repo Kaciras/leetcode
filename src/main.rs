7dd638bc6ab4214c9723d92af5cd1362bcf1eb7c
Kaciras <Kaciras@protonmail.com>
2024-10-12 13:48:42 +0800

    add 135

--- src/main.rs
+++ src/main.rs
@@ -39,9 +39,9 @@ mod q398_random_pick_index;
 mod q130_surrounded_regions;
 mod q1768_merge_strings_alternately;
 mod q682_baseball_game;
-mod q148_sort_list;
 mod q134_gas_station;
 mod q151_reverse_words_in_a_string;
+mod q135_candy;
 
 /// 检查有无做重复的题，以及题目的序号是否重了，至于序号错误就不管了。
 fn check() {
---
+++ src/q135_candy.rs
@@ -0,0 +1,42 @@
+pub struct Solution {}
+
+impl Solution {
+	// 正向扫一遍，遇上坡 +1，再反过来一遍，最后每个位置取最大值。
+	// 关于上坡下坡的都应当考虑使用两次扫描法。
+	pub fn candy(ratings: Vec<i32>) -> i32 {
+		let mut candies = vec![1; ratings.len()];
+		for i in 1..ratings.len() {
+			if ratings[i] > ratings[i - 1] {
+				candies[i] = candies[i - 1] + 1;
+			}
+		}
+		for i in (0..ratings.len() - 1).rev() {
+			if ratings[i] > ratings[i + 1]
+				&& candies[i] <= candies[i + 1]
+			{
+				candies[i] = candies[i + 1] + 1;
+			}
+		}
+		return candies.iter().sum();
+	}
+}
+
+#[cfg(test)]
+mod tests {
+	use super::*;
+
+	#[test]
+	fn case1() {
+		assert_eq!(Solution::candy(vec![1, 3, 4, 5, 2]), 11);
+	}
+
+	#[test]
+	fn example1() {
+		assert_eq!(Solution::candy(vec![1, 0, 2]), 5);
+	}
+
+	#[test]
+	fn example2() {
+		assert_eq!(Solution::candy(vec![1, 2, 2]), 4);
+	}
+}
