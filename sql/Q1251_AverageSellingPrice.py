92a2de936f4edc4c9df1daa091fa4130be86b4d5
Kaciras <Kaciras@protonmail.com>
2024-10-13 11:42:10 +0800

    add 596 and 1251

---
+++ sql/Q1251_AverageSellingPrice.py
@@ -0,0 +1,35 @@
+from sql_questions import define
+
+# COALESCE 可以把 NULL 转为指定的值，且支持表达式；ISNULL 仅支持列。
+# BETWEEN 是双闭区间，此题正好是相等的日期也算。
+
+sql_test = define("""
+SELECT Prices.product_id, COALESCE(ROUND(SUM(price * units) / SUM(units), 2), 0) AS average_price
+FROM Prices LEFT JOIN UnitsSold 
+ON Prices.product_id = UnitsSold.product_id AND purchase_date BETWEEN start_date AND end_date
+GROUP BY Prices.product_id
+""")
+
+@sql_test("""\
+Create table If Not Exists Prices (product_id int, start_date date, end_date date, price int);
+Create table If Not Exists UnitsSold (product_id int, purchase_date date, units int);
+Truncate table Prices;
+insert into Prices (product_id, start_date, end_date, price) values ('1', '2019-02-17', '2019-02-28', '5');
+insert into Prices (product_id, start_date, end_date, price) values ('1', '2019-03-01', '2019-03-22', '20');
+insert into Prices (product_id, start_date, end_date, price) values ('2', '2019-02-01', '2019-02-20', '15');
+insert into Prices (product_id, start_date, end_date, price) values ('2', '2019-02-21', '2019-03-31', '30');
+Truncate table UnitsSold;
+insert into UnitsSold (product_id, purchase_date, units) values ('1', '2019-02-25', '100');
+insert into UnitsSold (product_id, purchase_date, units) values ('1', '2019-03-01', '15');
+insert into UnitsSold (product_id, purchase_date, units) values ('2', '2019-02-10', '200');
+insert into UnitsSold (product_id, purchase_date, units) values ('2', '2019-03-22', '30');
+""")
+def test_example():
+	"""
+	+------------+---------------+
+	| product_id | average_price |
+	+------------+---------------+
+	| 1          | 6.96          |
+	| 2          | 16.96         |
+	+------------+---------------+
+	"""
---
+++ sql/Q596_ClassesMoreThan5Students.py
@@ -0,0 +1,30 @@
+from sql_questions import define
+
+# 用子查询也行，但内置的 HAVING 语句肯定更好。
+# sql_test = define("SELECT class FROM (SELECT class, COUNT(*) AS cnt FROM Courses GROUP BY class) _ WHERE cnt >= 5")
+
+# 因为 WHERE 在 GROUP BY 之前执行，所以无法过滤分组后的数据，于是就有 HAVING。
+# HAVING 子句可以让我们筛选分组后的数据。
+sql_test = define("SELECT class FROM Courses GROUP BY class HAVING COUNT(*) >= 5")
+
+@sql_test("""\
+Create table If Not Exists Courses (student varchar(255), class varchar(255));
+Truncate table Courses;
+insert into Courses (student, class) values ('A', 'Math');
+insert into Courses (student, class) values ('B', 'English');
+insert into Courses (student, class) values ('C', 'Math');
+insert into Courses (student, class) values ('D', 'Biology');
+insert into Courses (student, class) values ('E', 'Math');
+insert into Courses (student, class) values ('F', 'Computer');
+insert into Courses (student, class) values ('G', 'Math');
+insert into Courses (student, class) values ('H', 'Math');
+insert into Courses (student, class) values ('I', 'Math');
+""")
+def test_example():
+	"""
+	+---------+
+	| class   |
+	+---------+
+	| Math    |
+	+---------+
+	"""
