9e64eb862b513fdecab87b0ed2e5e5cef05b35bf
Kaciras <Kaciras@protonmail.com>
2024-10-17 13:50:25 +0800

    add 2633 and 1683

---
+++ sql/Q1633_PercentageOfUsersAttendedAContest.py
@@ -0,0 +1,52 @@
+from sql_questions import define
+
+sql_test = define("""
+SELECT 
+	contest_id, 
+	ROUND(COUNT(*) * 100 / (SELECT COUNT(*) FROM Users), 2) AS percentage 
+FROM 
+	Users 
+JOIN 
+	Register 
+ON 
+	Users.user_id = Register.user_id 
+GROUP BY 
+	contest_id 
+ORDER BY 
+	percentage DESC, contest_id
+""")
+
+
+@sql_test("""\
+Create table If Not Exists Users (user_id int, user_name varchar(20));
+Create table If Not Exists Register (contest_id int, user_id int);
+Truncate table Users;
+insert into Users (user_id, user_name) values ('6', 'Alice');
+insert into Users (user_id, user_name) values ('2', 'Bob');
+insert into Users (user_id, user_name) values ('7', 'Alex');
+Truncate table Register;
+insert into Register (contest_id, user_id) values ('215', '6');
+insert into Register (contest_id, user_id) values ('209', '2');
+insert into Register (contest_id, user_id) values ('208', '2');
+insert into Register (contest_id, user_id) values ('210', '6');
+insert into Register (contest_id, user_id) values ('208', '6');
+insert into Register (contest_id, user_id) values ('209', '7');
+insert into Register (contest_id, user_id) values ('209', '6');
+insert into Register (contest_id, user_id) values ('215', '7');
+insert into Register (contest_id, user_id) values ('208', '7');
+insert into Register (contest_id, user_id) values ('210', '2');
+insert into Register (contest_id, user_id) values ('207', '2');
+insert into Register (contest_id, user_id) values ('210', '7');
+""")
+def test_example():
+	"""
+	+------------+------------+
+	| contest_id | percentage |
+	+------------+------------+
+	| 208        | 100.0      |
+	| 209        | 100.0      |
+	| 210        | 100.0      |
+	| 215        | 66.67      |
+	| 207        | 33.33      |
+	+------------+------------+
+	"""
---
+++ sql/Q1683_InvalidTweets.py
@@ -0,0 +1,19 @@
+from sql_questions import define
+
+sql_test = define("SELECT tweet_id FROM Tweets WHERE LENGTH(content) > 15")
+
+
+@sql_test("""\
+Create table If Not Exists Tweets(tweet_id int, content varchar(50));
+Truncate table Tweets;
+insert into Tweets (tweet_id, content) values ('1', 'Let us Code');
+insert into Tweets (tweet_id, content) values ('2', 'More than fifteen chars are here!');
+""")
+def test_example():
+	"""
+	+----------+
+	| tweet_id |
+	+----------+
+	| 2        |
+	+----------+
+	"""
